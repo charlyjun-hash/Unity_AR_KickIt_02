241e4d5a86dff43ed1b3fdc21329d7c3e5078218
charlyjun-hash <carlosjunieles@gmail.com>
2026-04-16 16:19:26 -0500

    Nuevas modificaciones 16/04/2026

--- ProyectoC-CJ/Assets/GameControl.cs
+++ ProyectoC-CJ/Assets/GameControl.cs
@@ -8,6 +8,22 @@ public class GameControl : MonoBehaviour
     public float Fuerza = 10f;
     public Transform BalonTransform;
     public Transform DestinoTransform;
+
+    public Animator IndicadorAnimator;
+
+    public Transform TransformIndicador;
+    public float NivelDeFuerza1;
+    public float NivelDeFuerza2;
+    // capturar la posicion del indicador
+    public void DetenerBarraFuerza()
+    {
+        IndicadorAnimator.speed = 0;
+        IndicadorAnimator.Update(0);
+
+        NivelDeFuerza1 = TransformIndicador.localPosition.y;
+        NivelDeFuerza2 = Mathf.InverseLerp(0f, 2030f, NivelDeFuerza1);
+    }
+
     public void PatearPelota()
     {
         Vector3 direccion = (DestinoTransform.position  - BalonTransform.position).normalized;
